e6fe373e076fa6fe2c427ddfd0efe1c276baf924
James Palm <james@lunr.org>
2017-12-18 22:42:19 -0500

    Movie Model

---
+++ database/migrations/2017_12_19_024956_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user');
+            $table->string('title');
+            $table->string('format');
+            $table->integer('length');
+            $table->year('release_year');
+            $table->char('rating', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
