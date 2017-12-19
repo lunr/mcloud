5bd2e8488f84b82f919323730df7ae1f58cddb9e
James Palm <james@lunr.org>
2017-12-18 22:36:23 -0500

    Frontend blade layout

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+
+<title>mCloud - @yield('title')</title>
+
+<link rel="stylesheet" href="/css/app.css">
+
+<script src="/js/app.js"></script>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-default navbar-static-top">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/">
+        <img alt="Brand" src="/images/logo.jpg" width="32" height="32">
+      </a>
+    </div>
+    <ul class="nav navbar-nav">
+        <li><a href="/">Popular Movies</a></li>
+        <li><a href="/movies">My Movies</a></li>
+    </ul>
+  </div>
+</nav>
