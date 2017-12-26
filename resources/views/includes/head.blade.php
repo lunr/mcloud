4bff6233cffe37d25fc1f009cb8ce90459cfbfc3
James Palm <james@lunr.org>
2017-12-26 17:44:16 -0500

    Updating project name per specifications

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<title>mCloud - @yield('title')</title>
+<title>James Movie Manager - @yield('title')</title>
 
 <link rel="stylesheet" href="/css/app.css">
 
