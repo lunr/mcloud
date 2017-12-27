dc677ef185451134fb3bf073336fac95f93f9958
James Palm <james@lunr.org>
2017-12-26 23:02:56 -0500

    Styling :: responsive navigation

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-default navbar-static-top">
   <div class="container">
-    <div class="navbar-header">
+    <div class="navbar-header pull-left">
       <a class="navbar-brand" href="/">
         <img alt="Brand" src="/images/logo.jpg" width="32" height="32">
       </a>
     </div>
-    <ul class="nav navbar-nav">
+    <ul class="nav navbar-nav pull-right">
         <li><a href="/">Popular Movies</a></li>
         <li><a href="/movies">My Movies</a></li>
     </ul>
