f72b7fd32dc08952a01a3d8c7bd1af572f599236
James Palm <james@lunr.org>
2017-12-26 23:55:42 -0500

    Swap out logo, redirect login to home page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/movies';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="navbar-header pull-left">
       <a class="navbar-brand" href="/">
-        <img alt="Brand" src="/images/logo.jpg" width="32" height="32">
+        <img alt="Brand" src="/images/movie.png" width="32" height="32">
       </a>
     </div>
     <ul class="nav navbar-nav pull-right">
