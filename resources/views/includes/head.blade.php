8a496a8406a1e26ff2c1bf08a31d723bd0f79f77
James Palm <james@lunr.org>
2017-12-27 00:07:13 -0500

    Update head view to use APP_NAME

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<title>James Movie Manager - @yield('title')</title>
+<title>{{ env('APP_NAME') }} - @yield('title')</title>
 
 <link rel="stylesheet" href="/css/app.css">
 
