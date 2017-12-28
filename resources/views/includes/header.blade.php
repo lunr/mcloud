b70899375f339f43f70e56c89da150eba191c4d1
James Palm <james@lunr.org>
2017-12-28 15:55:24 -0500

    Only show primary navigation if user is logged in

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -5,9 +5,11 @@
         <img alt="Brand" src="/images/movie.png" width="32" height="32">
       </a>
     </div>
-    <ul class="nav navbar-nav pull-right">
-        <li><a href="/">Popular Movies</a></li>
-        <li><a href="/movies">My Movies</a></li>
-    </ul>
+    <?php if(Auth::user()) : ?>
+        <ul class="nav navbar-nav pull-right">
+            <li><a href="/">Popular Movies</a></li>
+            <li><a href="/movies">My Movies</a></li>
+        </ul>
+    <?php endif; ?>
   </div>
 </nav>
